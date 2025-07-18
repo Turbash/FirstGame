54e699b53916dc061d5025df96acf6f8345b0deb
Turbash Negi <negirawatdeepi@gmail.com>
2025-07-18 14:45:27 +0530

    Added level 2 with moving slime added wall jump feature which you have to use here without it you cannot win this level Will add level 3 now which will have a cannon

--- player.gd
+++ player.gd
@@ -2,6 +2,10 @@ extends CharacterBody2D
 
 const AIR_CONTROL_SPEED = 80.0
 const BOUNCE_VELOCITY = -300.0
+const WALL_JUMP_UPLIFT_VELOCITY=-300.0
+const WALL_PUSH_OFF_VElOCITY=-400.0
+var wall_jump_counter=0
+var direction=1
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -17,21 +21,14 @@ func _physics_process(delta):
 		velocity.y = BOUNCE_VELOCITY
 
 	# Get direction input (-1, 0, 1)
-	var direction = Input.get_axis("move_left", "move_right")
+	direction = Input.get_axis("move_left", "move_right")
 
 	# Sprite flipping (still useful mid-air)
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
 
-	# Animations
-	if is_on_floor():
-		animated_sprite.play("idle")  # Could change to "bounce" animation if you have one
-	else:
-		pass
+
 	# Movement control
 	if is_on_floor():
+		wall_jump_counter=0
 		velocity.x = 0  # Disable movement on ground
 	else:
 		# Limited mid-air control
@@ -39,5 +36,19 @@ func _physics_process(delta):
 			velocity.x = direction * AIR_CONTROL_SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0, AIR_CONTROL_SPEED)
+	
+	if is_on_wall():
+		if wall_jump_counter==0:
+			if Input.is_action_just_pressed("wall_jump"):	
+				var push_off_dir=-1 if animated_sprite.flip_h else 1
+				animated_sprite.flip_h=true if push_off_dir==1 else false
+				velocity.x=WALL_PUSH_OFF_VElOCITY*push_off_dir
+				velocity.y=WALL_JUMP_UPLIFT_VELOCITY
+				wall_jump_counter=1
+
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
 
 	move_and_slide()
